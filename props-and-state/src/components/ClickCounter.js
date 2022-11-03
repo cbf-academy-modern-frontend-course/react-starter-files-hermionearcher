110a9075816ac801d09bc1438e3653148089cf31
hermionearcher <hermione-archer@hotmail.co.uk>
2022-11-03 20:50:16 +0000

    button counter

---
+++ props-and-state/src/components/ClickCounter.js
@@ -0,0 +1,44 @@
+import React, { useState } from 'react'
+
+const ClickCounter = () => {
+    const [clickCount, setClickCount] = useState(0)
+
+    const handleClickInc = () => {
+        setClickCount(prevCount => prevCount + 1)
+    }
+    const handleClickDec = () => {
+        setClickCount(prevCount => prevCount - 1)
+    }
+    const buttonWrapperStyle = {
+        display: "flex"
+    }
+    const clickStyle = {
+        backgroundColor: "red",
+        border: "none",
+        borderRadius: "5px",
+        height: "7vh",
+        width: "15vh",
+        color: "white"
+    }
+    const unclickStyle = {
+        backgroundColor: "blue",
+        border: "none",
+        borderRadius: "5px",
+        height: "7vh",
+        width: "15vh",
+        color: "white"        
+    }
+
+  return (
+    <div>
+        <h1>Count: {clickCount}</h1>
+        <p>Click the button to increment the counter</p>
+        <div style={buttonWrapperStyle}>
+            <button style={clickStyle} onClick={handleClickInc}>Click#!</button>
+            <button style={unclickStyle} onClick={handleClickDec}>Unclick!</button>
+        </div>
+    </div>
+  )
+}
+
+export default ClickCounter
--- props-and-state/src/components/GadgetList.js
+++ props-and-state/src/components/GadgetList.js
@@ -1,11 +1,20 @@
 import React from 'react';
 
 const GadgetList = (props) => {
+  
+  const itemPriceStyle = {
+    borderBottom: "solid grey 1px",
+    color: "red"
+  }
 
   return (
     <main>
-      <h2>Headphones</h2>
-      {props.items.map((item) => (<div>{item.title}</div>))}
+      <h3>Headphones</h3>
+      {props.items.map((item) => (<div key={item.title}>
+        <h4>{item.title}</h4>
+        <h4 style={itemPriceStyle}>£{item.price}</h4>
+        </div>))}
+      
     </main>
   );
 }
