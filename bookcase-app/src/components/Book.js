9f4e7f2a9b3410becaa0f0b7f4859fce53a4d015
hermionearcher <hermione-archer@hotmail.co.uk>
2022-11-08 20:54:15 +0000

    more styling

--- bookcase-app/src/components/Book.js
+++ bookcase-app/src/components/Book.js
@@ -1,12 +1,23 @@
 import React from 'react';
+import './styles.css';
 
 function Book({ book, key }) {
   const truncate = (input) => (
-    input?.length > 202 ? `${input.substring(0,200)}...` : input
+    input?.length > 252 ? `${input.substring(0,250)}...` : input
   )
+  // const {
+  //   volumeInfo: {
+  //     title,
+  //     authors,
+  //     description,
+  //     imageLinks: { thumbnail}
+  //   },
+  //   saleInfo : { retailPrice,currencyCode }
+  // } = book
   const bookStyle = {
     display: "flex",
-    padding: 0,
+    padding: "1vh",
+    margin: 0,
     borderBottom: "1px solid lightgrey"
   }
   const bookCoverStyle = {
@@ -16,11 +27,26 @@ function Book({ book, key }) {
     display: "flex",
     flexDirection: "column",
     padding: 0,
-    marginLeft: "1vh"
+    marginLeft: "1vh",
+    alignSelf: "center",
   }
   const bookCoverWrapper = {
     alignSelf: "center",
     width: "15vh"
+  }
+  const bookAddButtonWrapper = {
+    alignSelf: "start",
+    width: "20vh",
+    paddingLeft: "1vh"
+  }
+  const bookAddButton = {
+    backgroundColor: "#0467fc",
+    color: "#fff",
+    border: "none",
+    padding: "8px 10px",
+    borderRadius: "5px",
+    fontSize: "1.1em",
+    minWidth: "80px"
   }
 
   return (
@@ -29,7 +55,7 @@ function Book({ book, key }) {
         <div style={bookCoverWrapper}>
           <img className='book-img' style={bookCoverStyle} alt={`${book.volumeInfo.title} book cover`} src={book.volumeInfo.imageLinks.thumbnail} />
         </div>
-        <div style={bookTextStyle}>
+        <div className="book-text" style={bookTextStyle}>
           <h2>{book.volumeInfo.title}</h2>
           {book.volumeInfo.authors.length > 1 ? (
             <p>{book.volumeInfo.authors.join(" and ")}</p>
@@ -41,6 +67,9 @@ function Book({ book, key }) {
             <p>{book.saleInfo.retailPrice.amount} <strong>{book.saleInfo.retailPrice.currencyCode}</strong></p>
           }
           <p>{truncate(book.volumeInfo.description)}</p>
+        </div>
+        <div style={bookAddButtonWrapper}>
+          <button style={bookAddButton}>Add +</button>
         </div>
       </div>
 
