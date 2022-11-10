c456de4b3397fd54a11f48d48ed1470985aa9cff
Hermione Archer <ARCHERH@yw.co.uk>
2022-11-10 19:26:02 +0000

    nesting components tasks

--- nesting-and-events/src/index.js
+++ nesting-and-events/src/index.js
@@ -1,29 +1,36 @@
 import React, { Fragment } from 'react';
 import ReactDOM from 'react-dom/client';
 import TaskContainer from './components/TaskContainer';
-/*
+import Task from './components/Task';
+
 const taskList = [
-  {"id":"task1","title":"Buy Milk","isComplete":true},
-  {"id":"task2","title":"Read a book","isComplete":false},
-  {"id":"task3","title":"Early morning run","isComplete":false},
-  {"id":"task4","title":"Drink water","isComplete":false},
-  {"id":"task5","title":"Visit the puppy shelter","isComplete":true},
-  {"id":"task6","title":"Watch Bad Sisters","isComplete":false}
+  { "id": "task1", "title": "Buy Milk", "isComplete": true },
+  { "id": "task2", "title": "Read a book", "isComplete": false },
+  { "id": "task3", "title": "Early morning run", "isComplete": false },
+  { "id": "task4", "title": "Drink water", "isComplete": false },
+  { "id": "task5", "title": "Visit the puppy shelter", "isComplete": true },
+  { "id": "task6", "title": "Watch Bad Sisters", "isComplete": false }
 ];
-*/
+
+const sortTasks = taskList.sort()
 
 function App() {
-  
-   return (
-      <Fragment>
-        <h1 key="heading">Task List</h1>
-        <TaskContainer>
-          {/* Task: Nesting Components - Add you tasks here: */}
-        </TaskContainer>
-      </Fragment>
+
+  return (
+    <Fragment>
+      <h1 key="heading">Task List</h1>
+      <TaskContainer>
+        {/* Task: Nesting Components - Add you tasks here: */}
+        {taskList.map((task, i) => {
+          return (
+            <Task key={i} id={task.id} title={task.title} isComplete={task.isComplete} />
+          )
+        })}
+      </TaskContainer>
+    </Fragment>
   );
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App/>);
+root.render(<App />);
 
