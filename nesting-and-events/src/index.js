cec4fbca05ab376f9e3023780684519fd89b69f2
hermionearcher <46831152+hermionearcher@users.noreply.github.com>
2022-11-10 20:40:03 +0000

    lifting state from child to parent
    callbacks

--- nesting-and-events/src/index.js
+++ nesting-and-events/src/index.js
@@ -1,15 +1,15 @@
-import React, { Fragment } from 'react';
-import ReactDOM from 'react-dom/client';
-import TaskContainer from './components/TaskContainer';
-import Task from './components/Task';
+import React, { Fragment } from "react";
+import ReactDOM from "react-dom/client";
+import TaskContainer from "./components/TaskContainer";
+import Task from "./components/Task";
 
 const taskList = [
-  { "id": "task1", "title": "Buy Milk", "isComplete": true },
-  { "id": "task2", "title": "Read a book", "isComplete": false },
-  { "id": "task3", "title": "Early morning run", "isComplete": false },
-  { "id": "task4", "title": "Drink water", "isComplete": false },
-  { "id": "task5", "title": "Visit the puppy shelter", "isComplete": true },
-  { "id": "task6", "title": "Watch Bad Sisters", "isComplete": false }
+  { id: "task1", title: "Buy Milk", isComplete: true },
+  { id: "task2", title: "Read a book", isComplete: false },
+  { id: "task3", title: "Early morning run", isComplete: false },
+  { id: "task4", title: "Drink water", isComplete: false },
+  { id: "task5", title: "Visit the puppy shelter", isComplete: true },
+  { id: "task6", title: "Watch Bad Sisters", isComplete: false },
 ];
 
 const getSortedElements = (sortByBool) => {
@@ -26,17 +26,21 @@ function App() {
     <Fragment>
       <h1 key="heading">Task List</h1>
       <TaskContainer>
-        {/* Task: Nesting Components - Add you tasks here: */}
         {taskList.map((task, i) => {
           return (
-            <Task key={i} id={task.id} title={task.title} isComplete={task.isComplete} toggleComplete={toggleComplete} />
-          )
+            <Task
+              key={i}
+              id={task.id}
+              title={task.title}
+              isComplete={task.isComplete}
+              toggleComplete={toggleComplete}
+            />
+          );
         })}
       </TaskContainer>
     </Fragment>
   );
 }
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
-
